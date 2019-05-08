71d08838b5aa75b6009d2db52e4cf461230b7019
snakehead007 <snakehead007@protonmail.com>
2019-05-08 15:07:49 +0200

    fixed issue start files

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 echo "updating as V1.6"
 echo "stashing..."
 git stash
-git "updating files..."
+echo "updating files..."
 git pull
 echo "updating npm packages.."
 npm install express jade mongodb mongoose forever forever-monitor chartjs --save
