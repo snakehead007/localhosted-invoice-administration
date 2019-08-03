1a69166ca48ea240bcce32b08cfccdc61ddb6020
snakehead007 <snakehead007@protonmail.com>
2019-08-03 18:17:24 +0200

    improved linux server start script

--- _linuxstart.bash
+++ _linuxstart.bash
@@ -4,21 +4,23 @@
 #run with sudo to ensure 'killall' command works, you might need sudo also on the 'monogd' command.
 
 clear
-echo "------------------------------------------------------";
-echo "-------------------STARTING MONGOD--------------------"
-echo "------------------------------------------------------";
-mongod &    #starts mongod as deamon
+printf "\n-------------------STARTING MONGOD--------------------\n"
+mongod --quiet &     #starts mongod as deamon
 sleep 3     #waits for connection start, no more than 3 seconds
-echo "------------------------------------------------------";
-echo "-------------------STARTING NODEJS--------------------"
-echo "------------------------------------------------------";
+printf "\n-------------------STARTING NODEJS--------------------\n"
 node . &    #starts node in directory (index.js)
-echo "------------------------------------------------------";
-echo "---------------MongoDB & NodeJS are up!---------------";
-echo "------------------------------------------------------";
-read -n1 -r -p "----------------Press any key to stop-----------------";    #when any key is pressed, the script will commence
-echo "------------------------------------------------------";
-echo "--------------Killing Node & Mongod-----------------";
-echo "------------------------------------------------------";
+printf "\n---------------MongoDB & NodeJS are up!---------------\n";
+read -n1 -r -p "\n----------------Press any key to stop-----------------\n";    #when any key is pressed, the script will commence
+
+printf "\n--------------Killing Node & Mongod-----------------\n";
 killall node;   #kills the node process
-killall mongod;   #kills the mongod process
+mongod --shutdown;   #kills the mongod process
+
+while true; do
+	read -p "Do you want to restart the script? (y/n) " yn
+    case $yn in
+        [Yy]* ) bash _linuxstart.bash; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
