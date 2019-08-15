e242e51c1ce4d0de92e23c4cd2497395f7875d50
snakehead007 <snakehead007@protonmail.com>
2019-08-15 18:35:37 +0200

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/logo.png 	modified:   views/nl/edit/edit-profile.jade 	modified:   views/nl/login.jade 	modified:   views/nl/pdf/pdf-eng.jade 	modified:   views/nl/pdf/pdf.jade 	new file:   views/nl/upload.jade 	modified:   .gitignore 	deleted:    _linuxstart.bash

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var multer = require('multer') // v1.0.5
+var upload = multer() // for parsing multipart/form-data
+const fileUpload = require('express-fileupload');
 var app = express();
+app.locals.title = 'Simple-invoice-administrator';
+app.locals.email = 'snakehead007@pm.me';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -12,6 +17,8 @@ mongoose.connect('mongodb://localhost:27017/sample-website');
 mongoose.connection.on('open', function() {
   console.log('Mongoose connected!');
 });
+
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/++[++^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
 var maand = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
 var maand_klein = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
@@ -4137,6 +4144,36 @@ app.post('/pass/:loginHash', function(req, res) {
 
 });
 
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/upload-logo/:loginHash', function (req, res) {
+    callFindPass().then(function(loginHash){
+    if (!(String(req.params.loginHash) === loginHash)) {
+      res.redirect('login');
+    }});
+    if (Object.keys(req.files).length == 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+    let sampleFile = req.files.sampleFile;
+    sampleFile.mv('public/logo.png', function(err) {
+    if (err)
+      console.log(err);
+
+    res.send('Logo uploaded! You can close this page.');
+  });
+})
+
+app.get('/upload/:loginHash',function(req,res){
+  callFindPass().then(function(loginHash){
+  if (!(String(req.params.loginHash) === loginHash)) {
+    res.redirect('login');
+  }});
+  res.render('nl/upload',{"loginHash":req.params.loginHash});
+
+});
+
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'jade');
@@ -4144,7 +4181,7 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen('3000', function() {
-  console.log('Server is running at PORT ' + 3000);
+  console.log('Server i s running at PORT ' + 3000);
   Schema = mongoose.Schema;
 
 });
