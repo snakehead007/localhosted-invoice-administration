772f43583f74c540a9dc62dcce489575668e9d7b
snakehead007 <snakehead007@protonmail.com>
2019-08-24 22:34:45 +0200

    fixed installations for windows

--- install.bat
+++ install.bat
@@ -1,31 +1,24 @@
-echo "Starting installation"
-echo "stashing..."
-git stash
-echo "updating files..."
-git pull
-echo "installing node files"
-node win-install.js
-echo "Installation is complete, you can close the program"
-pause
-//npm install -g express jade mongodb mongoose node-windows chartjs express-fileupload image-to-base64 --save
-echo "Installing npm packages";
-echo "Installing express";
+git stash;
+git pull;
 npm i -g express --save;
-echo "Installing jade";
+npm i express --save;
 npm i -g jade --save;
-echo "Installing mongodb";
+npm i jade --save;
 npm i mongodb -g --save;
-echo "Installing mongoose";
+npm i mongodb --save;
 npm i -g mongoose --save;
-echo "Installing node-windows";
+npm i mongoose --save;
 npm i -g node-windows --save;
-echo "Installing chartjs";
+npm i node-windows --save;
 npm i -g chartjs --save;
-echo "Installing express-fileupload";
+npm i chartjs --save;
 npm i -g express-fileupload --save;
-echo "Installing image-to-base64";
+npm i express-fileupload --save;
 npm i -g image-to-base64 --save;
-
-echo "Done installing npm packages";
-echo "Linking node-windows";
-npm link node-windows;  
+npm i image-to-base54 --save;
+npm i -g multer --save;
+npm i multer --save
+npm i pug --save;
+npm audit fix;
+npm link node-windows
+node win-install.js
