31057007cad7ca1f81ca155afcea81b01b30092b
snakehead007 <snakehead007@protonmail.com>
2019-10-01 11:30:47 +0200

    fixed windows and linux install files

--- install/windows/win-install.js
+++ install/windows/win-install.js
@@ -1,25 +1,21 @@
-echo "Starting node install";
 //npm install -g express jade mongodb mongoose node-windows chartjs express-fileupload image-to-base64 --save
-echo "Installing npm packages";
-echo "Installing express";
+
 npm i -g express --save;
-echo "Installing jade";
+
 npm i -g jade --save;
-echo "Installing mongodb";
+
 npm i mongodb -g --save;
-echo "Installing mongoose";
+
 npm i -g mongoose --save;
-echo "Installing node-windows";
+
 npm i -g node-windows --save;
-echo "Installing chartjs";
+
 npm i -g chartjs --save;
-echo "Installing express-fileupload";
+
 npm i -g express-fileupload --save;
-echo "Installing image-to-base64";
+
 npm i -g image-to-base64 --save;
 
-echo "Done installing npm packages";
-echo "Linking node-windows";
 npm link node-windows;
 
 var Service = require('node-windows').Service;
