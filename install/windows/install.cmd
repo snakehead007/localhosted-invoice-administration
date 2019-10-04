75db139b876430ea2de835d83e5688da16bf32ff
snakehead007 <snakehead007@protonmail.com>
2019-10-04 17:27:30 +0200

    installation files, updated

--- install/windows/install.cmd
+++ install/windows/install.cmd
@@ -1,6 +1,10 @@
 echo "Starting installation"
 git stash
 git pull
+echo "installing NPM/NPX"
+call .\node-v10.16.3-win-x64\npx.cmd
+echo "executing nodevars"
+call .\node-v10.16.3-win-x64\nodevars.bat
 node win-install.js
 npm link node-windows
 echo "Installation is complete, you can close the program"
--- install/windows/update.cmd
+++ install/windows/update.cmd
@@ -1,11 +1,6 @@
 echo "update simple-invoice-admisinstration"
-
 net stop simple-invoice-admisinstration
-
 git stash
-
 git pull
-
 net stop simple-invoice-admisinstration
-
 echo "update finished"
