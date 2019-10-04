f1c74c080e491ed7dcb2652ef82f9e609357873c
snakehead007 <snakehead007@protonmail.com>
2019-10-04 17:19:46 +0200

    adding node_modules for node binary

--- install/windows/install.cmd
+++ install/windows/install.cmd
@@ -2,6 +2,6 @@ echo "Starting installation"
 git stash
 git pull
 node win-install.js
-npm link node-windows //
+npm link node-windows
 echo "Installation is complete, you can close the program"
 pause
