20770337c1e1b8058fa9256676896fa19f442df0
BuildTools <karel@Karel.be>
2019-10-05 16:56:30 +0200

    fix for windows standalone (no node install)

--- install/windows/install.cmd
+++ install/windows/install.cmd
@@ -4,23 +4,31 @@ echo "Starting installation:"
 call node-v10.16.3-win-x64\nodevars.bat
 echo "[NPM]: installing express"
 call node-v10.16.3-win-x64\npm.cmd install -g express --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install express --save -s -q
 echo "[NPM]: pug"
 call node-v10.16.3-win-x64\npm.cmd install -g pug --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install pug --save -s -q
 echo "[NPM]: mongodb"
 call node-v10.16.3-win-x64\npm.cmd install mongodb -g --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install mongodb --save -s -q
 echo "[NPM]: mongoose"
 call node-v10.16.3-win-x64\npm.cmd install -g mongoose --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install mongoose --save -s -q
 echo "[NPM]: node-windows"
 call node-v10.16.3-win-x64\npm.cmd install -g node-windows --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install node-windows --save -s -q
 echo "[NPM]: chartjs"
 call node-v10.16.3-win-x64\npm.cmd install -g chartjs --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install chartjs --save -s -q
 echo "[NPM]: express-fileupload"
 call node-v10.16.3-win-x64\npm.cmd install -g express-fileupload --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install express-fileupload --save -s -q
 echo "[NPM]: image-to-base64"
 call node-v10.16.3-win-x64\npm.cmd install -g image-to-base64 --save -s -q
+call node-v10.16.3-win-x64\npm.cmd install image-to-base64 --save -s -q
 echo "installing packages - done!"
 echo "[NPM]: linking node-windows"
 call node-v10.16.3-win-x64\npm.cmd link node-windows -s -q
 echo "[NODE]: installing invoice-administration"
-node win-install.js
+call node-v10.16.3-win-x64\node.exe node win-install.js
 echo "Installation is complete, you can close the program"
