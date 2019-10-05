e572604343b1ef515bac90c3a6135e259b1db2fd
BuildTools <karel@Karel.be>
2019-10-05 15:45:19 +0200

    fix for windows standalone (no node install)

--- install/windows/install.cmd
+++ install/windows/install.cmd
@@ -1,10 +1,16 @@
 echo "Starting installation"
 git stash
 git pull
-echo "installing NPM/NPX"
-call .\node-v10.16.3-win-x64\npx.cmd
-echo "executing nodevars"
-call .\node-v10.16.3-win-x64\nodevars.bat
+call node-v10.16.3-win-x64\nodevars.bat
+npm install -g express --save;
+npm install -g jade --save;
+npm install mongodb -g --save;
+npm install -g mongoose --save;
+npm install -g node-windows --save;
+npm install -g chartjs --save;
+npm install -g express-fileupload --save;
+npm install -g image-to-base64 --save;
+npm link node-windows;
 node win-install.js
 npm link node-windows
 echo "Installation is complete, you can close the program"
--- install/windows/win-install.js
+++ install/windows/win-install.js
@@ -1,22 +1,4 @@
-//npm install -g express jade mongodb mongoose node-windows chartjs express-fileupload image-to-base64 --save
 
-npm i -g express --save;
-
-npm i -g jade --save;
-
-npm i mongodb -g --save;
-
-npm i -g mongoose --save;
-
-npm i -g node-windows --save;
-
-npm i -g chartjs --save;
-
-npm i -g express-fileupload --save;
-
-npm i -g image-to-base64 --save;
-
-npm link node-windows;
 
 var Service = require('node-windows').Service;
 
