431076de9b701a04220e0c6d07cd202582a96d1c
snakehead007 <snakehead007@protonmail.com>
2020-01-12 14:44:36 +0100

    workflow updated

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
 From node:12
-
 WORKDIR /usr/src/app
-
 COPY package*.json /usr/src/app/
-
 RUN npm install
 RUN npm audit fix
 COPY . /usr/src/app
 EXPOSE 3000
-
-ENV MONGO_HOST=mongodb
-
-
+ENV MONGO_HOST=mongo
 CMD [ "npm", "start" ]
--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,23 +1,15 @@
 const mongoose = require('mongoose');
 module.exports.default = async () => {
     try {
-        //const db_uri = process.env.MONGO_HOST + ":" + process.env.MONGO_PORT + "/" + process.env.MONGO_DB;
-        const db_uri="mongodb://"+process.env.MONGO_HOST+":27017/db";
-        await mongoose.connect(db_uri, {
+        console.log("[Info]: Trying to connect to "+process.env.DB_URI);
+        await mongoose.connect(process.env.DB_URI, {
             useCreateIndex: true,
             useUnifiedTopology: true,
             useNewUrlParser: true
-        }); //This is still on 'sample-website'. After automatisating all Data import and export, then will be changed
+        });
     } catch (e) {
-        //console.log('[Error]: Failed to connect to ' + process.env.MONGO_HOST + ":" + process.env.MONGO_PORT + ', trying ' + process.env.MONGO_HOST + ':' + process.env.MONGO_PORT_BACKUP);
-        //const db_uri = process.env.MONGO_HOST + ":" + process.env.MONGO_PORT_BACKUP + "/" + process.env.MONGO_DB;
-        console.log("[Error]: Failed to connect, trying on port 32777");
-        const db_uri = "mongodb://"+process.env.MONGO_HOST+":32777/db";
-        mongoose.connect(db_uri, {
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        }); //This is still on 'sample-website'. After automatisating all Data import and export, then will be changed
+        console.log("[Error]: Timout error, failed to connect to mongodb");
+        process.exit(1);
     }
     mongoose.connection.on('open', () => {
         console.log('[Info]: Mongoose connected!');
