8087576006ff62227cc2ed193dc096267b1bda8b
snakehead007 <snakehead007@protonmail.com>
2020-02-21 17:50:21 +0100

    code review on this file

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,11 +1,11 @@
-const Settings = require('../models/settings');
-const User = require('../models/user');
-const Profile = require('../models/profile');
-const fs = require('fs');
+const Settings = require("../models/settings");
+const User = require("../models/user");
+const Profile = require("../models/profile");
+const fs = require("fs");
 exports.uploadLogoGet = (req,res) => {
     Settings.findOne({fromUser:req.session._id}, function(err, settings) {
-        Profile.findOne({fromUser:req.session._id},async(err,profile) =>{
-            res.render('upload',{
+        Profile.findOne({fromUser:req.session._id},async(err,profile) => {
+            res.render("upload",{
                 "settings":settings,
                 "description":"Upload logo",
                 "profile":profile,
@@ -18,26 +18,26 @@ exports.uploadLogoGet = (req,res) => {
 exports.uploadLogoPost = async (req,res) => {
     try {
         if (Object.keys(req.files).length === 0) // TODO: THIS CHECK DOESNT WORK
-            return res.status(400).send('No files were uploaded.');
+            return res.status(400).send("No files were uploaded.");
         let logoFile = req.files.logoFile;
         if(logoFile.mimetype==="image/jpeg"||logoFile.mimetype==="image/jpg"||!logoFile.mimetype) {
             Profile.findOne({fromUser: req.session._id}, async function (err, profile) {
-                let url = 'public/images/' + req.session._id + '/logo.jpeg';
-                if (!fs.existsSync('public/images/' + req.session._id)) {
-                    await fs.mkdirSync('public/images/' + req.session._id);
+                let url = "public/images/" + req.session._id + "/logo.jpeg";
+                if (!fs.existsSync("public/images/" + req.session._id)) {
+                    await fs.mkdirSync("public/images/" + req.session._id);
                 }
                 await logoFile.mv(url);
                 profile.logoFile.data = fs.readFileSync(url);
-                profile.logoFile.contentType = 'image/jpeg';
+                profile.logoFile.contentType = "image/jpeg";
                 await profile.save();
-                req.flash('success', "succefully updated your logo");
-                res.redirect('/view/profile/');
+                req.flash("success", "succefully updated your logo");
+                res.redirect("/view/profile/");
             });
         }else{
-            req.flash('danger',"Wrong filetype");
+            req.flash("danger","Wrong filetype");
             throw new Error();
         }
     }catch(error){
-        res.redirect('/upload/logo');
+        res.redirect("/upload/logo");
     }
 };
