0e7e645a950e2c5d850ff9e7ffb35497e9262113
snakehead007 <snakehead007@protonmail.com>
2020-02-21 17:41:49 +0100

    code review on file

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -2,27 +2,25 @@ const Settings = require('../models/settings');
 const User = require('../models/user');
 const Profile = require('../models/profile');
 const fs = require('fs');
-exports.upload_logo_get = (req,res) => {
+exports.uploadLogoGet = (req,res) => {
     Settings.findOne({fromUser:req.session._id}, function(err, settings) {
-        Profile.findOne({fromUser:req.session._id},async(err,profile)=>{
+        Profile.findOne({fromUser:req.session._id},async(err,profile) =>{
             res.render('upload',{
                 "settings":settings,
                 "description":"Upload logo",
                 "profile":profile,
-                "role":(await User.findOne({_id:req.session._id},(err,user)=> {return user})).role
+                "role":(await User.findOne({_id:req.session._id},(err,user) => {return user})).role
             });
         });
     });
 };
 
-exports.upload_logo_post = async (req,res) => {
+exports.uploadLogoPost = async (req,res) => {
     try {
         if (Object.keys(req.files).length === 0) // TODO: THIS CHECK DOESNT WORK
             return res.status(400).send('No files were uploaded.');
         let logoFile = req.files.logoFile;
-        console.log(logoFile);
         if(logoFile.mimetype==="image/jpeg"||logoFile.mimetype==="image/jpg"||!logoFile.mimetype) {
-            console.log("[Info]: Upload logo " + logoFile.name + " from " + req.session._id);
             Profile.findOne({fromUser: req.session._id}, async function (err, profile) {
                 let url = 'public/images/' + req.session._id + '/logo.jpeg';
                 if (!fs.existsSync('public/images/' + req.session._id)) {
@@ -40,7 +38,6 @@ exports.upload_logo_post = async (req,res) => {
             throw new Error();
         }
     }catch(error){
-        console.trace(error);
         res.redirect('/upload/logo');
     }
 };
