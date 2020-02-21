6f43d77639a17fcd2897cdc68ba9065c22ea1330
snakehead007 <snakehead007@protonmail.com>
2020-02-21 17:07:59 +0100

    code review on file

--- src/loaders/images.js
+++ src/loaders/images.js
@@ -1,62 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const Profile = require('../models/profile');
+const fs = require("fs");
+const path = require("path");
+const Profile = require("../models/profile");
 
-/** Load all images from the database into public/[ID]/logo.jpeg
- *
- * @param app
- * @returns {Promise<void>}
- */
-module.exports.images = async function images(app) {
-    //delete everything in images
-    await deleteFolderRecursive('public/images');
-
-    //create new empty folder images
-    await fs.mkdirSync('public/images');
-
-    //restore all profile.logoFile in database
-    await Profile.find({},async function(err,profiles){
-       if(err) console.trace(err);
-       for(let profile of profiles){
-           //create folder for user
-           try {
-               await fs.mkdirSync('public/images/' + profile.fromUser);
-               if(process.env.LOGGING>2)
-                    console.log("[DEBUG]: mkdir public/images/"+ profile.fromUser);
-           }catch(error){
-               if(process.env.LOGGING>2)
-                    console.log("[Error]: Got error in making directory");
-               if(process.env.LOGGING>2)
-                    console.trace("[Error]: "+error);
-           }
-           //add logo to user folder
-           try{
-               if(typeof profile.logoFile.data !== "undefined" || !profile.logoFile) {
-                   await fs.writeFile('public/images/' + profile.fromUser+'/logo.jpeg', profile.logoFile.data, function (err) {
-                       if (err) {
-                           if(process.env.LOGGING>2){
-                               console.trace("[Error]: "+err);
-                           }
-                           throw err;
-                       }
-                       if(!err){
-                           if(process.env.LOGGING>2)
-                                console.log("[DEBUG]: mkdir public/images/"+ profile.fromUser+"/logo.jpeg");
-                       }
-                   });
-               }
-           }catch(e){
-               if(process.env.LOGGING>2)
-                    console.trace("[Error]: "+e);
-           }
-       }
-    });
-
-    ///TODO: check if any folder is empty delete them
-    /// ...
-    if(process.env.LOGGING>1)
-        console.log('[Info]: Wrote images/, all logo of profile written to disk');
-};
 
 const deleteFolderRecursive = function(_path) {
     if (fs.existsSync(_path)) {
@@ -65,13 +10,46 @@ const deleteFolderRecursive = function(_path) {
             if (fs.lstatSync(curPath).isDirectory()) { // recurse
                 deleteFolderRecursive(curPath);
             } else { // delete file
-                if(process.env.LOGGING>2)
-                    console.log("[DEBUG]: rm "+curPath);
                 fs.unlinkSync(curPath);
             }
         });
-        if(process.env.LOGGING>2)
-            console.log("[DEBUG]: rm "+_path);
         fs.rmdirSync(_path);
     }
+};
+
+/** Load all images from the database into public/[ID]/logo.jpeg
+ *
+ * @param app
+ * @returns {Promise<void>}
+ */
+module.exports.images = async function images(app) {
+    //delete everything in images
+    await deleteFolderRecursive("public/images");
+
+    //create new empty folder images
+    await fs.mkdirSync("public/images");
+
+    //restore all profile.logoFile in database
+    await Profile.find({},async function(err,profiles){
+       if(err) console.trace(err);
+       for(let profile of profiles){
+           //create folder for user
+           try {
+               await fs.mkdirSync("public/images/" + profile.fromUser);
+           }catch(error){
+           }
+           //add logo to user folder
+           try{
+               if(typeof profile.logoFile.data !== "undefined" || !profile.logoFile) {
+                   await fs.writeFile("public/images/" + profile.fromUser+"/logo.jpeg", profile.logoFile.data, function (err) {
+                       if (err) {
+                           throw err;
+                       }
+                   });
+               }
+           }catch(e){
+
+           }
+       }
+    });
 };
