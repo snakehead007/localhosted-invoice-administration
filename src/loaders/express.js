25a657ae9ea0c383c6f2f661e845d0e0dbfec80a
snakehead007 <snakehead007@protonmail.com>
2020-02-21 18:21:01 +0100

    added privacy policy

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,26 +1,24 @@
-const express = require( 'express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const pug = require('pug');
-const session = require('express-session');
-const flash = require('express-flash');
-const fileUpload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
+const express = require( "express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const pug = require("pug");
+const session = require("express-session");
+const flash = require("express-flash");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 const i18n = require("i18n");
 module.exports.default = function(app){
-    app.locals.title = 'invoice-administration';
-    app.locals.email = 'snakehead007@pm.me';
-    app.engine('pug', pug.__express);
-    app.set('views', path.join(path.resolve(), 'views'));
-    app.set('view engine', 'pug');
-    if(process.env.LOGGING>1)
-        console.log("[Info]: . . . . Engine and view loaded");
+    app.locals.title = "invoice-administration";
+    app.locals.email = "snakehead007@pm.me";
+    app.engine("pug", pug.__express);
+    app.set("views", path.join(path.resolve(), "views"));
+    app.set("view engine", "pug");
     app.use(cookieParser());
     i18n.configure({
-        locales:['en-GB', 'nl-BE'],
-        defaultLocale: 'nl-BE',
-        directory: path.join(path.resolve(),'locales'),
-        cookie:'session'
+        locales:["en-GB", "nl-BE"],
+        defaultLocale: "nl-BE",
+        directory: path.join(path.resolve(),"locales"),
+        cookie:"session"
     });
     app.use(function(req,res,next){
         i18n.init(req,res,next);
@@ -31,19 +29,15 @@ module.exports.default = function(app){
         };
         next();
     });
-    if(process.env.LOGGING>1)
-        console.log("[Info]: . . . . Locales set up");
     app.use(fileUpload());
-    app.use(express.static(path.join(path.resolve(), 'public')));
+    app.use(express.static(path.join(path.resolve(), "public")));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
         extended: true
     }));
-    if(process.env.LOGGING>1)
-        console.log("[Info]: . . . . Express settings set up");
     app.use(
         session({
-            name: 'session',
+            name: "session",
             secret: process.env.SESSION_SECRET,
             saveUninitialized: false,
             resave: false
@@ -54,10 +48,5 @@ module.exports.default = function(app){
         res.locals.session = req.session;
         next();
     });
-    if(process.env.LOGGING>1)
-        console.log("[Info]: . . . . Sessions set up");
-    app.listen(process.env.PORT,() => {
-        if(process.env.LOGGING>0)
-            console.log('[Info]: Server is running at PORT ' + process.env.PORT);
-    });
+    app.listen(process.env.PORT);
 };
