b46b0d5c4bb7b6211e8d92448935a41aa1fc5a79
snakehead007 <snakehead007@protonmail.com>
2020-03-08 15:39:27 +0100

    fixed core bugs

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -24,7 +24,8 @@ exports.valueMustBeEmail = (req, res, doc, mustBeFilledIn = false, message = "Th
 };
 
 exports.numberMustPhoneNumber = (req, res, doc, mustBeFilledIn = false, message = "Phone number is invalid, please provide a correct phone number") => {
-    let invalid = checkRegex(doc, /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/u,mustBeFilledIn);
+    let c_phoneNr = doc.replace(/\.+/g,'').replace(/\\+/g,'').replace(/\-+/g,'').replace(/\s+/g,'').replace(/\/+/g,'');
+    let invalid = checkRegex(c_phoneNr, /^[\+0]{0,1}[0-9]{0,12}$/u,mustBeFilledIn);
     return checkForMessage(req, invalid,message);
 };
 
